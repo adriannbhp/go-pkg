bcdd8d20385c81b78e2e0134b14a8fbd53150c67
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-11-13 15:31:42 +0700

    update "tambahan"

--- peda_test.go
+++ peda_test.go
@@ -9,11 +9,72 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func TestUpdateGetData(t *testing.T) {
-	mconn := SetConnection("MONGOULBI", "petapedia")
-	datagedung := GetAllBangunanLineString(mconn, "petapedia")
-	fmt.Println(datagedung)
+func TestCreateNewUserRole(t *testing.T) {
+	var userdata User
+	userdata.Username = "adrian"
+	userdata.Password = "bener"
+	userdata.Role = "admin"
+	mconn := SetConnection("MONGOSTRING", "gisajah")
+	CreateNewUserRole(mconn, "users", userdata)
 }
+
+func TestDeleteUser(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "petapedia")
+	var userdata User
+	userdata.Username = "yyy"
+	DeleteUser(mconn, "user", userdata)
+}
+
+func CreateNewUserToken(t *testing.T) {
+	var userdata User
+	userdata.Username = "dzikri"
+	userdata.Password = "dzikri"
+	userdata.Role = "admin"
+
+	// Create a MongoDB connection
+	mconn := SetConnection("MONGOSTRING", "petapedia")
+
+	// Call the function to create a user and generate a token
+	err := CreateUserAndAddToken("your_private_key_env", mconn, "user", userdata)
+
+	if err != nil {
+		t.Errorf("Error creating user and token: %v", err)
+	}
+}
+
+func TestGFCPostHandlerUser(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "petapedia")
+	var userdata User
+	userdata.Username = "dzikri"
+	userdata.Password = "dzikri"
+	userdata.Role = "admin"
+	CreateNewUserRole(mconn, "user", userdata)
+}
+
+func TestProduct(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "petapedia")
+	var productdata Product
+	productdata.Nomorid = 1
+	productdata.Name = "dzikri"
+	productdata.Description = "haq"
+	productdata.Price = 1000
+	productdata.Size = "XL"
+	productdata.Stock = 100
+	productdata.Image = "https://images3.alphacoders.com/165/thumb-1920-165265.jpg"
+	CreateNewProduct(mconn, "product", productdata)
+}
+
+func TestAllProduct(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "petapedia")
+	product := GetAllProduct(mconn, "product")
+	fmt.Println(product)
+}
+
+// func TestUpdateGetData(t *testing.T) {
+// 	mconn := SetConnection("MONGOULBI", "petapedia")
+// 	datagedung := GetAllBangunanLineString(mconn, "petapedia")
+// 	fmt.Println(datagedung)
+// }
 
 func TestGeneratePasswordHash(t *testing.T) {
 	password := "adrian"
@@ -34,13 +95,13 @@ func TestGeneratePrivateKeyPaseto(t *testing.T) {
 }
 
 func TestHashFunction(t *testing.T) {
-	mconn := SetConnection("MONGOULBI", "gisajah")
+	mconn := SetConnection("MONGOSTRING", "gisajah")
 	var userdata User
 	userdata.Username = "adrian"
 	userdata.Password = "adrian"
 
 	filter := bson.M{"username": userdata.Username}
-	res := atdb.GetOneDoc[User](mconn, "user", filter)
+	res := atdb.GetOneDoc[User](mconn, "users", filter)
 	fmt.Println("Mongo User Result: ", res)
 	hash, _ := HashPassword(userdata.Password)
 	fmt.Println("Hash Password : ", hash)
@@ -57,4 +118,23 @@ func TestIsPasswordValid(t *testing.T) {
 
 	anu := IsPasswordValid(mconn, "user", userdata)
 	fmt.Println(anu)
+}
+
+func CreateContent(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "petapedia")
+	var contentdata Content
+	contentdata.ID = 1
+	contentdata.Content = "dzikri"
+	contentdata.Description = "haq"
+	contentdata.Image = "https://images3.alphacoders.com/165/thumb-1920-165265.jpg"
+	CreateNewContent(mconn, "content", contentdata)
+}
+
+func TestUserFix(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "petapedia")
+	var userdata User
+	userdata.Username = "dzikri"
+	userdata.Password = "dzikri"
+	userdata.Role = "admin"
+	CreateUser(mconn, "user", userdata)
 }
